53798005bc65014c89a239c609b30d518a691161
Mikhail <derecs@mail.ru>
2021-07-11 13:50:08 +0300

    add test view

--- source/company_list/src/main/java/ru/amk/company_list/CompanyListActivity.kt
+++ source/company_list/src/main/java/ru/amk/company_list/CompanyListActivity.kt
@@ -1,11 +1,11 @@
-package ru.amk.stocktipsapp
+package ru.amk.company_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CompanyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_company_list)
     }
 }
