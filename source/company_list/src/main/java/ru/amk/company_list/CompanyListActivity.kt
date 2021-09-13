a5467ae0e807b45ed76427a1ea9847246d3ce896
Mikhail <derecs@mail.ru>
2021-09-13 23:49:15 +0300

    refactoring

--- source/company_list/src/main/java/ru/amk/company_list/CompanyListActivity.kt
+++ source/company_list/src/main/java/ru/amk/company_list/CompanyListActivity.kt
@@ -22,6 +22,7 @@ import ru.amk.company_list.di.DaggerCompanyListComponent
 import ru.amk.company_list.list.CompanyListAdapter
 import ru.amk.company_list.list.CompanyListPresenter
 import ru.amk.company_list.list.CompanyListViewImpl
+import ru.amk.company_list.repository.HeaderViewState
 import ru.amk.core.di.AppWithFacade
 import ru.amk.core.di.DaggerCoreComponent
 import javax.inject.Inject
@@ -42,7 +43,7 @@ class CompanyListActivity : AppCompatActivity() {
     private val secidOrderRightImageButton: ImageButton by lazy { findViewById(R.id.secid_order_right_image_button) }
     private val nameOrderReverseImageButton: ImageButton by lazy { findViewById(R.id.name_order_reverse_image_button) }
     private val secidOrderReverseImageButton: ImageButton by lazy { findViewById(R.id.secid_order_reverse_image_button) }
-    private val toolbar: Toolbar by lazy {  findViewById(R.id.toolbar) }
+    private val toolbar: Toolbar by lazy { findViewById(R.id.toolbar) }
 
     companion object {
         fun startCompanyListActivity(context: Context) {
@@ -86,8 +87,6 @@ class CompanyListActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("InflateParams", "ResourceAsColor")
     private fun sorted() {
-
-
         with(Settings) {
             sortByNameTextView.setOnClickListener {
                 sortedBy = SortedBy.NAME
@@ -123,9 +122,7 @@ class CompanyListActivity : AppCompatActivity() {
                     update()
                 }
             }
-
         }
-
     }
 
 
@@ -144,101 +141,47 @@ class CompanyListActivity : AppCompatActivity() {
         companyListPresenter.sortBy(Settings.sortedBy, Settings.orderBy, Settings.favoriteUp)
         companyListRW.scrollToPosition(0)
         with(Settings) {
-            when (stateSorting) {
-                SortHandler.StateSort.NAME_RIGHT_FAV_TRUE, SortHandler.StateSort.NAME_RIGHT_FAV_FALSE -> {
-                    sortByNameTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.dark
-                        )
-                    )
-                    sortBySecidTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.gray600
-                        )
-                    )
-                    nameOrderRightImageButton.setImageResource(R.drawable.arrow_down_select)
-                    secidOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    nameOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                    secidOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                }
+            updateViewsHeader()
+        }
+    }
 
-                SortHandler.StateSort.NAME_REVERSE_FAV_TRUE, SortHandler.StateSort.NAME_REVERSE_FAV_FALSE -> {
-                    sortByNameTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.dark
-                        )
-                    )
-                    sortBySecidTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.gray600
-                        )
-                    )
-                    nameOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    secidOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    nameOrderReverseImageButton.setImageResource(R.drawable.arrow_up_select)
-                    secidOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                }
-                SortHandler.StateSort.SEC_ID_RIGHT_FAV_TRUE, SortHandler.StateSort.SEC_ID_RIGHT_FAV_FALSE -> {
-                    sortByNameTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.gray600
-                        )
-                    )
-                    sortBySecidTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.dark
-                        )
-                    )
-                    nameOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    secidOrderRightImageButton.setImageResource(R.drawable.arrow_down_select)
-                    nameOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                    secidOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                }
-                SortHandler.StateSort.SEC_ID_REVERSE_FAV_TRUE, SortHandler.StateSort.SEC_ID_REVERSE_FAV_FALSE -> {
-                    sortByNameTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.gray600
-                        )
-                    )
-                    sortBySecidTextView.setTextColor(
-                        ContextCompat.getColor(
-                            applicationContext,
-                            R.color.dark
-                        )
-                    )
-                    nameOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    secidOrderRightImageButton.setImageResource(R.drawable.arrow_down_not_select)
-                    nameOrderReverseImageButton.setImageResource(R.drawable.arrow_up_not_select)
-                    secidOrderReverseImageButton.setImageResource(R.drawable.arrow_up_select)
-                }
+    private fun Settings.updateViewsHeader() {
+        val headerViewState = HeaderViewState(this@CompanyListActivity)
+        when (stateSorting) {
+            SortHandler.StateSort.NAME_RIGHT_FAV_TRUE,
+            SortHandler.StateSort.NAME_RIGHT_FAV_FALSE -> {
+                headerViewState.nameRight()
+            }
+            SortHandler.StateSort.NAME_REVERSE_FAV_TRUE,
+            SortHandler.StateSort.NAME_REVERSE_FAV_FALSE -> {
+                headerViewState.nameRevers()
+            }
+            SortHandler.StateSort.SEC_ID_RIGHT_FAV_TRUE,
+            SortHandler.StateSort.SEC_ID_RIGHT_FAV_FALSE -> {
+                headerViewState.secIdRight()
+            }
+            SortHandler.StateSort.SEC_ID_REVERSE_FAV_TRUE,
+            SortHandler.StateSort.SEC_ID_REVERSE_FAV_FALSE -> {
+                headerViewState.secIdReverse()
             }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
-        menu?.let{
-
+        menu?.let {
             val search: MenuItem = it.findItem(R.id.action_search)
             val searchView = search.actionView as SearchView
             searchView.queryHint = "Название или тикер"
-            searchView.setOnQueryTextListener(object :
-                SearchView.OnQueryTextListener {
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
                 override fun onQueryTextSubmit(query: String?): Boolean {
-                    query?.let { companyListPresenter.filterCompany(query)}
+                    query?.let { companyListPresenter.filterCompany(query) }
                     return false
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-                    newText?.let { companyListPresenter.filterCompany(newText)}
+                    newText?.let { companyListPresenter.filterCompany(newText) }
                     return false
                 }
             })
@@ -247,7 +190,7 @@ class CompanyListActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.action_search) {
+        if (item.itemId == R.id.action_search) {
             Toast.makeText(this, R.string.search_hint, Toast.LENGTH_SHORT).show()
         }
         return true
@@ -266,7 +209,6 @@ class CompanyListActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         companyListPresenter.onCleared()
-
     }
 
 }
